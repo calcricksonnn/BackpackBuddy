ebdf3e9b6f754f40b38676cd48643c86aa5da2ac
calcricksonnn <calcrickson22@icloud.com>
2025-07-16 13:02:32 +0800

    Create routeplanner.js

---
+++ routeplanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function RoutePlanner() {
+  return (
+    <View style={styles.container}>
+      <Text>Route Planner & Travel Calendar screen coming soon 🗺️</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex:1, justifyContent:'center', alignItems:'center' },
+});
