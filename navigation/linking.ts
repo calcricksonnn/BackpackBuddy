10355bd96f40a4851beabc44efcca2632b39e5c9
calcricksonnn <calcrickson22@icloud.com>
2025-08-02 11:57:39 +0800

    Create linking.ts

---
+++ navigation/linking.ts
@@ -0,0 +1,13 @@
+export const linking = {
+  prefixes: ['backpackbuddy://'],
+  config: {
+    screens: {
+      Explore: 'explore',
+      Journey: 'journey',
+      Meetups: 'meetups',
+      Inbox: 'inbox',
+      Profile: 'profile',
+      Onboarding: 'onboarding'
+    }
+  }
+};
