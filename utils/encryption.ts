085f22f59d2a383b716ffba30577e079ae800b50
calcricksonnn <calcrickson22@icloud.com>
2025-08-02 11:52:31 +0800

    Update encryption.ts

--- utils/encryption.ts
+++ utils/encryption.ts
@@ -1,7 +1,7 @@
-export const encryptMessage = (plain: string): string => {
+export const encrypt = (plain: string): string => {
   return Buffer.from(plain).toString('base64');
 };
 
-export const decryptMessage = (encoded: string): string => {
+export const decrypt = (encoded: string): string => {
   return Buffer.from(encoded, 'base64').toString('utf-8');
 };
