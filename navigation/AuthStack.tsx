d8f0b972c3733d7f7f31a6680f8e93a067d3a4e1
calcricksonnn <calcrickson22@icloud.com>
2025-08-02 17:20:47 +0800

    Update AuthStack.tsx

--- navigation/AuthStack.tsx
+++ navigation/AuthStack.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { OnboardingScreen } from '../screens/Onboarding/OnboardingScreen';
+import { LoginScreen } from '../screens/LoginScreen';
+import { RegisterScreen } from '../screens/RegisterScreen'; // you'll add this next
 
 const Stack = createNativeStackNavigator();
 
 export const AuthStack: React.FC = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
+  <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Onboarding">
     <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-    {/* Add LoginScreen, RegisterScreen later if needed */}
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );
