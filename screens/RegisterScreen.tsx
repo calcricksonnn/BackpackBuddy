52357fe1b45bedae07575d5cce957fa40063f4d2
calcricksonnn <calcrickson22@icloud.com>
2025-08-02 19:41:28 +0800

    Update RegisterScreen.tsx

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -38,7 +38,7 @@ export const RegisterScreen: React.FC = () => {
 
   return (
     <ImageBackground
-      source={require('../../assets/onboarding/IMG_1240.jpeg')}
+      source={require('../../assets/Onboarding/IMG_1240.jpeg')}
       style={styles.bg}
       resizeMode="cover"
     >
