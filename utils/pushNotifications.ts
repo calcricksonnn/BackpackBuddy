d774405d69d1cafdfc8daa4a35d93f24ad2a976a
calcricksonnn <calcrickson22@icloud.com>
2025-08-02 12:17:41 +0800

    Create pushNotifications.ts

---
+++ utils/pushNotifications.ts
@@ -0,0 +1,20 @@
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Platform } from 'react-native';
+
+export const registerForPushNotifications = async (): Promise<string | null> => {
+  if (!Device.isDevice) return null;
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== 'granted') return null;
+
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+  return token;
+};
