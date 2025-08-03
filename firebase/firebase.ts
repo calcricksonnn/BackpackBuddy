431f646d0d8c05b6d087d3f978320fb1ced8710f
calcricksonnn <calcrickson22@icloud.com>
2025-08-03 17:18:12 +0800

    Update firebase.ts

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,19 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: "AIzaSyAxUycd_saBQgknRucniBhcSiGIh5eKzSg",
+  authDomain: "backpackbuddy-d4569.firebaseapp.com",
+  projectId: "backpackbuddy-d4569",
+  storageBucket: "backpackbuddy-d4569.appspot.com",
+  messagingSenderId: "240634133130",
+  appId: "1:240634133130:web:bab2d039160932a44b4b73"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { app, auth };
