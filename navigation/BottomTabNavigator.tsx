b3c16f2166cc51077e4f6ef4a4d1fe44f7325f95
calcricksonnn <calcrickson22@icloud.com>
2025-08-03 23:03:09 +0800

    Create BottomTabNavigator.tsx

---
+++ navigation/BottomTabNavigator.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+import ExploreScreen from '../screens/ExploreScreen';
+import MapScreen from '../screens/MapScreen';
+import MeetupsScreen from '../screens/MeetupsScreen';
+import InboxScreen from '../screens/InboxScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          borderTopColor: '#eee',
+          height: 65,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          marginBottom: 5,
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconName: any;
+
+          if (route.name === 'Explore') iconName = 'compass-outline';
+          else if (route.name === 'Map') iconName = 'map-outline';
+          else if (route.name === 'Meetups') iconName = 'calendar-outline';
+          else if (route.name === 'Inbox') iconName = 'chatbubble-ellipses-outline';
+          else if (route.name === 'Profile') iconName = 'person-circle-outline';
+
+          return <Ionicons name={iconName} size={24} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: '#888',
+      })}
+    >
+      <Tab.Screen name="Explore" component={ExploreScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Meetups" component={MeetupsScreen} />
+      <Tab.Screen name="Inbox" component={InboxScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
