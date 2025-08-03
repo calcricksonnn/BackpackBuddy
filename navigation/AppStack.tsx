458f1850b78a2027f715d8f438d41659c29679f6
calcricksonnn <calcrickson22@icloud.com>
2025-08-03 23:04:10 +0800

    Update AppStack.tsx

--- navigation/AppStack.tsx
+++ navigation/AppStack.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import ExploreScreen from '../screens/ExploreScreen';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Explore" component={ExploreScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="HomeTabs" component={BottomTabNavigator} />
+    </Stack.Navigator>
   );
 }
