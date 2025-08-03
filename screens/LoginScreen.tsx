e1dc65c0a34824a5096b3a29aac209e1e4bc69ba
calcricksonnn <calcrickson22@icloud.com>
2025-08-03 11:55:42 +0800

    Update LoginScreen.tsx

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -26,10 +26,10 @@ export const LoginScreen: React.FC = () => {
 
   return (
     <ImageBackground
-  source={{ uri: 'https://imgur.com/a/pSdxWRL' }}
+  source={require('../../../assets/onboarding/bg2.png')}
   style={styles.bg}
   resizeMode="cover"
->
+/>
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
